d385fae3f05b1e39eb8d545ed8d3e973c3ab6b2c
fcxmarquez <francisco.marquez.solt@gmail.com>
2022-09-29 14:44:37 -0500

    webpack condig

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+import { App } from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
